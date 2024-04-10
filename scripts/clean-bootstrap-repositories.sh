4ae8ab4368d7b03de77121ea06642a67a79ff533
Cédric Bellegarde <cedric.bellegarde@adishatz.org>
2024-04-10 15:41:40 +0200

    scripts: clean up adishatz repo

--- scripts/clean-bootstrap-repositories.sh
+++ scripts/clean-bootstrap-repositories.sh
@@ -9,8 +9,10 @@ EOF
 # Remove extrepos
 rm -f /etc/apt/sources.list.d/extrepo_droidian.sources
 rm -f /etc/apt/sources.list.d/extrepo_mobian.sources
+rm -f /etc/apt/sources.list.d/extrepo_adishatz.sources
 rm -f /var/lib/extrepo/keys/hybris-mobian.asc
 rm -f /var/lib/extrepo/keys/mobian.asc
+rm -f /var/lib/extrepo/keys/adishatz.asc
 
 # Nuke /etc/apt/sources.list
 > /etc/apt/sources.list
